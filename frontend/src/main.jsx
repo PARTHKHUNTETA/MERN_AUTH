a43ddb6d3fe10b8c3868358e7ff0f9434d7e0d15
Parth Khunteta <parthkhunteta@Parths-MacBook-Air.local>
2023-12-26 14:49:32 +0530

    added the statemangement routes and apislice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,31 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import store from "./store.js";
+import { Provider } from "react-redux";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index path="/" element={<HomeScreen />} />
+      <Route index path="/login" element={<LoginScreen />} />
+      <Route index path="/register" element={<RegisterScreen />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
