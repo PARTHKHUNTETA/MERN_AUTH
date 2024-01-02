7e765c22b8b92335dfbe2aa350891071cb47d967
Parth Khunteta <parthkhunteta@192.168.1.3>
2024-01-02 21:57:07 +0530

    addedcode for production

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config()
 const PORT = process.env.PORT || 5000;
@@ -15,7 +16,15 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/users', userRoutes)
 
-app.get('/', (req, res) => { res.send('SERVER IS READY') })
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')) })
+}
+else {
+    app.get('/', (req, res) => { res.send('SERVER IS READY') })
+}
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(PORT, () => {
